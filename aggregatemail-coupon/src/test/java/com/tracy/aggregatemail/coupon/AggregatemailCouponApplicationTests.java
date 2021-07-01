02e987bf6b93b1cc69d38aaadee996c0870158ae
fgzs <fgzsmyf@gmail.com>
2021-07-02 03:58:48 +0800

    Project First Create

---
+++ aggregatemail-coupon/src/test/java/com/tracy/aggregatemail/coupon/AggregatemailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracy.aggregatemail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatemailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ aggregatemail-order/src/test/java/com/tracy/aggregatemail/order/AggregatemailOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracy.aggregatemail.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatemailOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
